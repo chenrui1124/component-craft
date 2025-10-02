da1a919501a233d45097b9c1eb1cc1377e76a85e
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-10-03 00:12:31 +0800

    Add default layout & set import alias

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from 'astro/config'
+import { fileURLToPath } from 'node:url'
 
 import tailwindcss from '@tailwindcss/vite'
-
-import react from '@astrojs/react';
-import vue from '@astrojs/vue';
+import react from '@astrojs/react'
+import vue from '@astrojs/vue'
 
 export default defineConfig({
+  integrations: [react(), vue()],
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
   },
-
-  integrations: [react(), vue()],
 })
