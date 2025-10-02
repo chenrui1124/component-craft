518a132e13c85ccfdcf0f4bdd08f09d0b33171f7
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-10-03 00:10:12 +0800

    Setup react & vue framework dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -1,3 +1,14 @@
 import { defineConfig } from 'astro/config'
 
-export default defineConfig({})
+import tailwindcss from '@tailwindcss/vite'
+
+import react from '@astrojs/react';
+import vue from '@astrojs/vue';
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react(), vue()],
+})
